8a0b589ce3ac7afe6ef1965bf800decd49cbcee6
Hadrian Bayanulhaq S <hadrianbayanulhaq@gmail.com>
2016-05-26 20:13:34 +0700

    Add 3 sensor collab with AND comparator
    3 sensor collab with AND comparator, checking if each sensor satisfies
    the selected rule for collaboration.

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -304,7 +304,7 @@ class Api extends CI_Controller {
 								#Prepare the Data for insertion to DATABASE
 								$newCollabData = array(
 								'sensor_x_value' => $sensor_x_value[0]->datareading,
-								'sensor_y_value' => $sensor_reading[0]->datareading
+								'sensor_y_value' => $sensor_reading[0]->datareading,
 								'sensor_z_value' => $sensor_reading,
 								'sensor_collab_id' => $collabs->sensor_collab_id,
 								'sensor_reading' => $result
@@ -317,7 +317,169 @@ class Api extends CI_Controller {
 						}
 						elseif($collabs->comp_operator == 'AND')
 						{
+							#INCOMING DATA FROM X
+							if($collabs->sensor_x_id == $sensor_id[0]->SENSORID)
+							{
+								#Get Y sensor rule id.
+								$sensorCollabRuleId_y = $collabs->sensor_y_rule_id;
+								#Get Z sensor rule id.
+								$sensorCollabRuleId_z = $collabs->sensor_z_rule_id;
 
+								#get the latest Y value
+								$sensor_y_value = $this->Data_model->getLastRecord($collabs->sensor_y_id);
+								#get latest Z value
+								$sensor_z_value = $this->Data_model->getLastRecord($collabs->sensor_z_id);
+
+								#Compare the last y result to RULE stored in database
+								#IF RULE IS SATISFIED -> COMPARE Z RULE
+								if($this->Api_model->checkCollabSensorRule($sensorCollabRuleId_y, $sensor_y_value[0]->datareading))
+								{
+									#Compare Last Z result to RULE stored in database. If TRUE, start calculating.
+									if($this->Api_model->checkCollabSensorRule($sensorCollabRuleId_z, $sensor_z_value[0]->datareading))
+									{
+										#CALCULATION BLOCK
+										$x = $sensor_reading;
+										$y = $sensor_y_value[0]->datareading;
+										$z = $sensor_z_value[0]->datareading;
+										$collabMathExpression = $collabs->operator;
+										$replace = array("x", "y", "z");
+										$replaceWith = array($x, $y, $z);
+										$mathExpressionEval = str_replace($replace, $replaceWith, $collabMathExpression);
+										$result = eval('return '.$mathExpressionEval.';');
+										#ENDOF CALCULATION BLOCK
+
+										#prepare data for insertion
+										$newCollabData = array(
+											'sensor_x_value' => $sensor_reading,
+											'sensor_y_value' => $sensor_y_value[0]->datareading,
+											'sensor_z_value' => $sensor_z_value[0]->datareading,
+											'sensor_collab_id' => $collabs->sensor_collab_id,
+											'sensor_reading' => $result
+											);
+										echo "AND, Y, Z SATISFIED". "\n";
+
+										#Insert to database
+										print_r($newCollabData);
+									}
+									else
+									{
+										echo "Z not Satisfied\n";
+									}
+								}
+								else
+								{
+									echo "Y Not Satisfied\n";
+								}
+							}
+
+							#INCOMING DATA FROM Y
+							if($collabs->sensor_y_id == $sensor_id[0]->SENSORID)
+							{
+								#Get X sensor rule id.
+								$sensorCollabRuleId_x = $collabs->sensor_x_rule_id;
+								#Get Z sensor rule id.
+								$sensorCollabRuleId_z = $collabs->sensor_z_rule_id;
+
+								#get the latest X value
+								$sensor_x_value = $this->Data_model->getLastRecord($collabs->sensor_x_id);
+								#get latest Z value
+								$sensor_z_value = $this->Data_model->getLastRecord($collabs->sensor_z_id);
+
+								#Compare the last X result to RULE stored in database
+								#IF RULE IS SATISFIED -> COMPARE Z RULE
+								if($this->Api_model->checkCollabSensorRule($sensorCollabRuleId_x, $sensor_x_value[0]->datareading))
+								{
+									#Compare Last Z result to RULE stored in database. If TRUE, start calculating.
+									if($this->Api_model->checkCollabSensorRule($sensorCollabRuleId_z, $sensor_z_value[0]->datareading))
+									{
+										#CALCULATION BLOCK
+										$x = $sensor_x_value[0]->datareading;
+										$y = $sensor_reading;
+										$z = $sensor_z_value[0]->datareading;
+										$collabMathExpression = $collabs->operator;
+										$replace = array("x", "y", "z");
+										$replaceWith = array($x, $y, $z);
+										$mathExpressionEval = str_replace($replace, $replaceWith, $collabMathExpression);
+										$result = eval('return '.$mathExpressionEval.';');
+										#ENDOF CALCULATION BLOCK
+
+										#prepare data for insertion
+										$newCollabData = array(
+											'sensor_x_value' => $sensor_x_value[0]->datareading,
+											'sensor_y_value' => $sensor_reading,
+											'sensor_z_value' => $sensor_z_value[0]->datareading,
+											'sensor_collab_id' => $collabs->sensor_collab_id,
+											'sensor_reading' => $result
+											);
+										echo "AND, X, Z SATISFIED". "\n";
+
+										#Insert to database
+										print_r($newCollabData);
+									}
+									else
+									{
+										echo "Z not Satisfied\n";
+									}
+								}
+								else
+								{
+									echo "X Not Satisfied\n";
+								}
+							}
+							#INCOMING DATA FROM Z
+							if($collabs->sensor_z_id == $sensor_id[0]->SENSORID)
+							{
+								#Get X sensor rule id.
+								$sensorCollabRuleId_x = $collabs->sensor_x_rule_id;
+								#Get Y sensor rule id.
+								$sensorCollabRuleId_y = $collabs->sensor_y_rule_id;
+
+								#get the latest X value
+								$sensor_x_value = $this->Data_model->getLastRecord($collabs->sensor_x_id);
+								#get latest Y value
+								$sensor_y_value = $this->Data_model->getLastRecord($collabs->sensor_y_id);
+
+								#Compare the last X result to RULE stored in database
+								#IF RULE IS SATISFIED -> COMPARE Z RULE
+								if($this->Api_model->checkCollabSensorRule($sensorCollabRuleId_x, $sensor_x_value[0]->datareading))
+								{
+									#Compare Last Z result to RULE stored in database. If TRUE, start calculating.
+									if($this->Api_model->checkCollabSensorRule($sensorCollabRuleId_y, $sensor_y_value[0]->datareading))
+									{
+										#CALCULATION BLOCK
+										$x = $sensor_x_value[0]->datareading;
+										$y = $sensor_y_value[0]->datareading;
+										$z = $sensor_reading;
+										$collabMathExpression = $collabs->operator;
+										$replace = array("x", "y", "z");
+										$replaceWith = array($x, $y, $z);
+										$mathExpressionEval = str_replace($replace, $replaceWith, $collabMathExpression);
+										$result = eval('return '.$mathExpressionEval.';');
+										#ENDOF CALCULATION BLOCK
+
+										#prepare data for insertion
+										$newCollabData = array(
+											'sensor_x_value' => $sensor_x_value[0]->datareading,
+											'sensor_y_value' => $sensor_y_value[0]->datareading,
+											'sensor_z_value' => $sensor_reading,
+											'sensor_collab_id' => $collabs->sensor_collab_id,
+											'sensor_reading' => $result
+											);
+										echo "AND, X, Y SATISFIED". "\n";
+
+										#Insert to database
+										print_r($newCollabData);
+									}
+									else
+									{
+										echo "Y not Satisfied\n";
+									}
+								}
+								else
+								{
+									echo "X Not Satisfied\n";
+								}
+							}
 						}
 
 					}
